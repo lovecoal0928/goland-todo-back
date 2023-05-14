39420b94892f020a04f613b847d01ec2fa4feb08
coa3 <lovecoal928@gmail.com>
2023-05-15 00:25:25 +0900

    add controller files

--- routes/v1.route.go
+++ routes/v1.route.go
@@ -1,6 +1,10 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"go-todo-back/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func v1Route(app *fiber.App) {
 
@@ -8,26 +12,10 @@ func v1Route(app *fiber.App) {
 
 	todo := v1.Group("/todo")
 
-	todo.Post("/", func(c *fiber.Ctx) error {
-		// create one todo
-		return nil
-	})
+	todo.Post("/", controllers.CreateTodo)
+	todo.Get("/", controllers.GetAllTodo)
 
-	todo.Get("/", func(c *fiber.Ctx) error {
-		// get list of todo
-		return nil
-	})
-
-	todo.Get("/:id", func(c *fiber.Ctx) error {
-		// get todo by id
-		return nil
-	})
-	todo.Patch("/:id", func(c *fiber.Ctx) error {
-		// update todo by id
-		return nil
-	})
-	todo.Delete("/:id", func(c *fiber.Ctx) error {
-		// delete todo by id
-		return nil
-	})
+	todo.Get("/:id", controllers.GetTodoByID)
+	todo.Patch("/:id", controllers.UpdateTodoByID)
+	todo.Delete("/:id", controllers.DeleteTodoByID)
 }
